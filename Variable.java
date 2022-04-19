405a8391a727c5f165e87e2c040a4eccdeb437e3
samrat tayade <36564519+samrattayade@users.noreply.github.com>
2022-04-19 22:06:27 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,18 @@
+// Variables in java
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int variable_name= 20;
+		System.out.println(variable_name);
+		
+		int a= 25;
+		int b= 24;
+		int c= a+b;
+		
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		System.out.println("c: a+b= "+c);
+	}
+
+}
