7c132bc52611af13eb89e8fdff8dd435b554ad0b
samrat tayade <36564519+samrattayade@users.noreply.github.com>
2022-04-19 21:18:45 +0530

    Create comment.java

---
+++ comment.java
@@ -0,0 +1,11 @@
+//Single Line Comment
+public class HelloWorld {
+     public static void main(String args[]) {
+    	 System.out.println("Hello World!");
+     }
+}
+/*
+multi line 
+comment
+here is comment
+*/
